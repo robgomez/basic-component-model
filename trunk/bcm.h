83ae3bf6efa83d72a4609ce3662a14cd71cbdb11
arteks2001@gmail.com <arteks2001@gmail.com@ea963f4a-9555-be2a-12fe-edc49bb6fb60>
2011-12-13 23:58:11 +0000

    Basic Component Model (BCM) main header file.

---
+++ trunk/bcm.h
@@ -0,0 +1,21 @@
+#ifndef BCM_H
+#define BCM_H
+
+/**
+@file bcm.h
+@brief Basic Component Model (BCM) main header file.
+
+This file includes all the required BCM header files.
+*/
+
+#include <bcm/identifiable.h>
+#include <bcm/identifier.h>
+#include <bcm/identifierdata.h>
+#include <bcm/interface.h>
+#include <bcm/component.h>
+#include <bcm/win32/dll.h>
+#include <bcm/win32/componentmgr.h>
+
+#endif
+
+
